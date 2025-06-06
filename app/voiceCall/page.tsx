acaafd0c7b34a1be4be21221242c1e6815a507c0
Prathamesh Desai <prathameshdesai679@gmail.com>
2025-06-07 00:42:18 +0530

    voiceCall

--- app/voiceCall/page.tsx
+++ app/voiceCall/page.tsx
@@ -5,7 +5,7 @@ import VoiceCallTimer from "@/components/VoiceCallTimer";
 export default function Page() {
   return (
     <div className="w-full h-full max-w-[300px] max-h-[30vh] mx-auto my-8 relative">
-      <VoiceCallTimer redirectTo="/problem" durationMinutes={0.25} />
+      <VoiceCallTimer redirectTo="/problem" durationMinutes={5} />
       <VoiceCallWidget className="w-full h-full" />
     </div>
   );
